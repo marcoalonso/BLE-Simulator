1dbd9622e5591d732b9ec217d7ba32e1d862c707
Marco Alonso <marcoalonsorota@hotmail.com>
2024-10-04 12:41:06 -0600

    Initial Commit

---
+++ BeaconEmitter2/BeaconEmitter2App.swift
@@ -0,0 +1,17 @@
+//
+//  BeaconEmitter2App.swift
+//  BeaconEmitter2
+//
+//  Created by Marco Alonso on 04/10/24.
+//
+
+import SwiftUI
+
+@main
+struct BeaconEmitter2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
