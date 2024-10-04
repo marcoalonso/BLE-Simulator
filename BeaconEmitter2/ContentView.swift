a40e5794b536106d83132ff6f49010bf0e174597
Marco Alonso <marcoalonsorota@hotmail.com>
2024-10-04 12:56:39 -0600

    BLE Ready

--- BeaconEmitter2/ContentView.swift
+++ BeaconEmitter2/ContentView.swift
@@ -6,14 +6,20 @@
 //
 
 import SwiftUI
+import CoreBluetooth
+import CoreLocation
 
 struct ContentView: View {
+    @StateObject private var beaconClass = BeaconClass()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image("ble")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 200, height: 200)
+            Text("This is an BLE Simulator")
+                .font(.title)
         }
         .padding()
     }
@@ -21,4 +27,55 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+
+class BeaconClass: NSObject, ObservableObject, CBPeripheralManagerDelegate {
+    private var peripheralManager: CBPeripheralManager?
+    private let region: CLBeaconRegion
+    var centralManager: CBCentralManager!
+    
+    @Published var discoveredBeacons: [CBPeripheral] = []
+    
+    override init() {
+        let uuid = UUID(uuidString: "B9407F30-F5F8-466E-AFF9-25556B57FE6D")!
+        region = CLBeaconRegion()
+        super.init()
+        peripheralManager = CBPeripheralManager(delegate: self, queue: nil)
+    }
+    
+    func startAdvertising() {
+        guard let peripheralManager = peripheralManager else {
+            return // Ensure peripheralManager is not nil
+        }
+        if peripheralManager.state == .poweredOn {
+            let peripheralData = region.peripheralData(withMeasuredPower: nil) as! [String: Any]
+            let additionalData: [String: Any] = [CBAdvertisementDataLocalNameKey: "iPhone BLE", CBAdvertisementDataServiceUUIDsKey: [CBUUID(string: "180D" )]]
+            let mergedData = peripheralData.merging(additionalData) { (_, new) in new }
+            peripheralManager.startAdvertising(mergedData)
+            print ("advertising started")
+        } else {
+            print ("Peripheral manager is not in powered-on state")
+        }
+    }
+    
+    func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
+        switch peripheral.state {
+            
+        case .unknown:
+            print("Debug: unknown")
+        case .resetting:
+            print("Debug: resetting")
+        case .unsupported:
+            print("Debug: unsupported")
+        case .unauthorized:
+            print("Debug: unauthorized")
+        case .poweredOff:
+            peripheral.stopAdvertising()
+        case .poweredOn:
+            startAdvertising()
+        @unknown default:
+            print("Debug: default")
+        }
+    }
 }
